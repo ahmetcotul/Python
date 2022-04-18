84205890698a5c84677ae275c55e29c14c9c93c0
ahmetcotul <83516837+ahmetcotul@users.noreply.github.com>
2022-04-18 03:28:59 +0300

    Add files via upload

---
+++ lambdaFunction.py
@@ -0,0 +1,62 @@
+"""
+@author: ahmet
+
+*lambda functions are functions without a name
+*Unlike functions with "def()" and "return" syntax,lambda returns an expression without a "return" statement
+*Lambda can be defined anywhere a function is expected without having to assign a variable
+
+
+If we want to write a function that takes the cube;
+
+y2 and y functions values ​​work the same.
+    
+"""
+
+
+
+y = lambda x : x ** 3
+print(y(8))
+
+
+
+def y2(x):
+    x **= 3
+    return x
+print(y2(8))
+
+
+
+print(y2(5)==y(5))
+
+
+
+'''
+[Out]:
+    512
+    512
+    True
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
