e5c13eac12ec2e5a4d58584f95d6bbd2b103aa24
ahmetcotul <83516837+ahmetcotul@users.noreply.github.com>
2022-04-19 07:07:47 +0300

    Update trim.py

--- trim.py
+++ trim.py
@@ -2,6 +2,11 @@
 
 greeting = "xxxHello Worldyyyy"
 
+#right and left trim
 print("with strip:",greeting.strip("xy"))
+
+#right trim
 print("with rstrip:",greeting.rstrip("xy"))
+
+#left trim
 print("with lstrip:",greeting.lstrip("xy"))
