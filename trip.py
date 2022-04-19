ffbd7d3b77d30c518d2844153963cda8a28f8325
ahmetcotul <83516837+ahmetcotul@users.noreply.github.com>
2022-04-19 07:01:19 +0300

    Add files via upload

---
+++ trip.py
@@ -0,0 +1,7 @@
+#Python Trip for String
+
+greeting = "xxxHello Worldyyyy"
+
+print("with strip:",greeting.strip("xy"))
+print("with rstrip:",greeting.rstrip("xy"))
+print("with lstrip:",greeting.lstrip("xy"))
