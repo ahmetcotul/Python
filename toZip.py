87870a2c28f3a0ffdab9ca6adad2750f8176888d
ahmetcotul <83516837+ahmetcotul@users.noreply.github.com>
2022-04-19 07:21:27 +0300

    Add files via upload

---
+++ toZip.py
@@ -0,0 +1,18 @@
+
+a = ["Harry","Potter",76]
+b = ["Gandalf","Hobbit",87]
+
+x = list(zip(a,b))
+
+print(x)
+
+
+
+#len if not equal
+a = ["Harry","Potter",76]
+b = ["Gandalf","Hobbit"]
+
+
+x = list(zip(a,b))
+
+print(x)
